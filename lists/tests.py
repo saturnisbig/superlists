a1a42ddd59587d543e34085ae51e07f4f204e4cc
Teddy <i.kenting@gmail.com>
2014-04-19 15:32:02 +0800

    FT now could check post two new item

--- lists/tests.py
+++ lists/tests.py
@@ -17,3 +17,16 @@ class HomePageTest(TestCase):
         resp = home_page(req)
         expected_html = render_to_string('home.html')
         self.assertEqual(resp.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        resp = home_page(request)
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': request.POST['item_text']}
+        )
+
+        self.assertEqual(expected_html, resp.content.decode())
